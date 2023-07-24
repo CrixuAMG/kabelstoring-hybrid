1568bc08fd4fcd8aa221e9bf24acf5b120694d6a
Christian Kaal <christiankaal@gmail.com>
2023-07-24 21:30:09 +0200

    Cleaned code, refactored setting password

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -3,13 +3,14 @@
 namespace App\Actions\Fortify;
 
 use Laravel\Fortify\Rules\Password;
+use Illuminate\Contracts\Validation\Rule;
 
 trait PasswordValidationRules
 {
     /**
      * Get the validation rules used to validate passwords.
      *
-     * @return array<int, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<int, Rule|array|string>
      */
     protected function passwordRules(): array
     {
